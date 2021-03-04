64003557d59d0aa73d3f084f1f2a92d08c088d4c
Ritik Srivastava <ritik.srivastava@mayadata.io>
2021-03-04 18:53:43 +0530

    save local changes
    Signed-off-by: Ritik Srivastava <ritik.srivastava@mayadata.io>

--- src/lab/Graphs/LineAreaGraph/LineAreaGraph.tsx
+++ src/lab/Graphs/LineAreaGraph/LineAreaGraph.tsx
@@ -1,4 +1,4 @@
-import ParentSize from "@visx/responsive/lib/components/ParentSize";
+import { ParentSize } from "@visx/visx";
 import React from "react";
 import { LineAreaGraphProps } from "./base";
 import { FilteredLineAreaGraph } from "./FilteredLineAreaGraph";
